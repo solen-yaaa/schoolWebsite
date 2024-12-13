d6964fab93dc36b0015da0dd54bfe0b02218e194
Cecilia Schreiber <cecilia.schreiber@nhochdrei.de>
2024-12-13 11:54:23 +0100

    function Amount,Price & const

--- scripts/order.js
+++ scripts/order.js
@@ -5,7 +5,20 @@ function getSelectValue() {
     return document.getElementById('select').value;   
 }
 
+function getAmount() {
+    return document.getElementById('amount').value;   
+}
+
+function getPrice() {
+    return "25";
+}
+
 function addOrder() {
-    const value = getSelectValue(); 
-    console.log(value);
+   const value = getSelectValue(); 
+   var productname = getSelectValue();
+   var price = getPrice();
+   var amount = getAmount();
+   console.log(amount, productname, price);
+   const table = document.getElementById('tableorder');
+   const row = document.createElement('tr');
 }
